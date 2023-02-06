8d54efb5fde742662767320ea148e7a14ebd2cf5
khorakova <horakoak@gmail.com>
2023-02-06 09:56:24 +0100

    Řešení

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // tady je místo pro náš program
+let vystup = document.querySelector(".vystup");
+
+let jmeno = prompt("Zadej své jméno");
+let vek = Number(prompt("Zadej svůj věk")); 
+
+vystup.innerHTML = jmeno +", věk: " + vek;
+
+let hodinovaMzda = Number(prompt("Zadej svou hodinovou sazbu v Kč"));
+let hodinyZaDen = Number(prompt("Kolik hodin denně pracuješ?"));
+let dniVMesici = Number(prompt("Kolik dní v měsíci pracuješ?"));
+
+let mzda = document.querySelector(".mzda");
+let vypocetMzdy = hodinovaMzda * hodinyZaDen * dniVMesici;
+
+mzda.innerHTML = "Hrubá mzda: "+ vypocetMzdy; 
+
